fb0b7bfd9ee1d49d6cea4d270f5176c9daa31771
Junior-Ngoune <ngounewamba@gmail.com>
2023-11-17 22:00:48 -0500

    fonction analyser commande

--- phase1.py
+++ phase1.py
@@ -3,7 +3,7 @@ import argparse
 
 
 def analyser_commande():
-    parser = argparse.ArgumentParser(description="Créationggg du menu pour aller chercher les symboles et les dates ")
+    parser = argparse.ArgumentParser(description="Création du menu pour aller chercher les symboles et les dates ")
     parser.add_argument('-symbole', metavar='Symboles...', type=str, nargs='+', help="Nom d'un symbole boursier")
     parser.add_argument('-h', '--help', action='help', help="show this help message and exit")
     parser.add_argument('-d', '--date-debut', metavar='DATE', type=datetime, help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
