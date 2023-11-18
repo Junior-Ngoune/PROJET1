b4434fbd734ccc86bd99c5721fe6791e23e2203a
Junior-Ngoune <ngounewamba@gmail.com>
2023-11-17 21:44:20 -0500

    test

--- phase1.py
+++ phase1.py
@@ -3,12 +3,13 @@ import argparse
 
 
 def analyser_commande():
-    parser = argparse.ArgumentParser(description="Création du menu pour aller chercher les symboles et les dates ")
-    parser.add_argument('-h', '--aide', metavar='AIDE', action='AIDE', help="Afin de pouvoir obtenir de l'aide",)
-    parser.add_argument('-d', '--date-debut', metavar='DATE DE DEBUT', help="Afin de pouvoir préciser une date de début")
-    parser.add_argument('-f', '--date-fin', metavar='DATE DE FIN', help="Afin de pouvoir préciser une date de fin")
-    parser.add_argument('-v', '--valeur', metavar='Valeur', choices=['fermeture', 'ouverture', 'min', 'max', 'volume'], help="Afin de pouvoir choisir la valeur désirée")
-
+    parser = argparse.ArgumentParser(description="Créationggg du menu pour aller chercher les symboles et les dates ")
+    parser.add_argument('-symbole', metavar='Symboles...', type=str, nargs='+', help="Nom d'un symbole boursier")
+    parser.add_argument('-h', '--help', action='help', help="show this help message and exit")
+    parser.add_argument('-d', '--date-debut', metavar='DATE', type=datetime, help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
+    parser.add_argument('-f', '--date-fin', metavar='DATE', type=datetime, help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
+    parser.add_argument('-v', '--valeur', metavar='Valeur', type=str, choices=['fermeture', 'ouverture', 'min', 'max', 'volume'], default='fermeture', help="La valeur désirée (par défaut: fermeture)")
+    return parser.parse_args()
 #def produire_historique():
 
 
